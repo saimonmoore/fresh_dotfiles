e4fb79e7098c3ccfaa436c8c4f00ff83755e1592
Saimon Moore <saimonmoore@gmail.com>
2021-01-31 00:51:12 +0100

    Adding dev setup based on ansible

---
+++ system/setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function install_ansible() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    which brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install ansible
+  fi
+}
+
+function install_ansible_plugins() {
+  ansible-galaxy install cimon-io.asdf
+}
+
+which ansible-playbook > /dev/null
+
+if [ "$?" != "0" ]; then
+  install_ansible
+  install_ansible_plugins
+fi
+
+# provision machine
+ansible-playbook -i "localhost," -c local --become-method=su playbook.yml
