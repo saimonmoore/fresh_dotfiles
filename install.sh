5c1a5f79ca5c3117f3da8aed3774c3f299085169
Saimon Moore <saimonmoore@gmail.com>
2021-02-01 08:08:32 +0100

    Adding post-install instructions

--- install.sh
+++ install.sh
@@ -11,3 +11,17 @@ echo "info: Provisioning machine"
 echo "info: Setting up dotfiles via fresh"
 FRESH_LOCAL_SOURCE=saimonmoore/fresh_dotfiles bash <(curl -sL get.freshshell.com)
 
+echo "info: Setting up TPM"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+grep fresh ~/.bashrc || echo "source ~/.fresh/build/shell.sh" >> ~/.bashrc
+
+echo "info: Installation complete! Now open up a new shell and complete post-installation tasks:"
+
+echo "info: Install all tmux plugins"
+echo "run: In tmux, hit prefix + I to install plugins"
+
+echo "info: Install all vim plugins"
+echo "run: In nvim, hit :PluginInstall to install plugins"
+
+
