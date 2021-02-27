63f9514abb209bad7fafe748b61dbd1ad7ccb34d
Saimon Moore <saimonmoore@gmail.com>
2021-02-27 17:55:30 +0100

    Install nvm + rbenv

--- install.sh
+++ install.sh
@@ -8,21 +8,37 @@ cd dotfiles/system
 echo "info: Provisioning machine"
 ./setup.sh
 
-echo "info: Setting up dotfiles via fresh"
-FRESH_LOCAL_SOURCE=saimonmoore/fresh_dotfiles bash <(curl -sL get.freshshell.com)
-
 echo "info: Setting up TPM"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+rm -fr ~/.tmux/plugins/tpm && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+echo "info: Installing nvm"
+rm -fr ~/.nvm
+git clone https://github.com/nvm-sh/nvm.git .nvm
+cd ~/.nvm && git checkout v0.37.2
+. ./nvm.sh
+cd -
+
+echo "info: Installing rbenv"
+if [ ! -d "$HOME/.rbenv" ]
+then
+  curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+fi
+
+echo "info: Setting up dotfiles via fresh"
+rm -fr ~/.freshrc && ~/.fresh && ~/.dotfiles && rm -fr ~/.config/nvim && FRESH_LOCAL_SOURCE=saimonmoore/fresh_dotfiles bash <(curl -sL get.freshshell.com)
 grep fresh ~/.bashrc || echo "source ~/.fresh/build/shell.sh" >> ~/.bashrc
 
 echo "info: Installation complete! Now open up a new shell and complete post-installation tasks:"
 
+echo "info: Install node:"
+echo "nvm install node"
+
+echo "info: Install ruby:"
+echo "rbenv install 2.7.1"
+
 echo "info: Install all tmux plugins"
 echo "run: In tmux, hit prefix + I to install plugins"
 
-echo "info: Install all vim plugins"
+echo "info: Ensure node + ruby are installed and then install all vim plugins"
 sh -c "curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 echo "run: In nvim, hit :PluginInstall and :UpdateRemotePlugins to install plugins"
-
-
