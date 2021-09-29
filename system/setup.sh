8c164f74753d53ddbbd484d8afd89ff52d4e3413
Saimon Moore <saimonmoore@gmail.com>
2021-09-29 08:15:20 +0200

    Don't install docker from ansible galaxy

--- system/setup.sh
+++ system/setup.sh
@@ -17,7 +17,6 @@ function install_ansible_plugins() {
   ansible-galaxy collection install community.general
   ansible-galaxy install markosamuli.asdf
   ansible-galaxy install darkwizard242.googlechrome
-  ansible-galaxy install geerlingguy.docker
   ansible-galaxy install lean_delivery.mysql
   ansible-galaxy install robertdebock.postgres
 }
