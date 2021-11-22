35efddcbb42dfcf1b61375d69ffaeb839fdd0dbf
Saimon Moore <saimonmoore@gmail.com>
2021-11-22 16:26:22 +0100

    tmp comment post ansible scripts

--- install.sh
+++ install.sh
@@ -34,22 +34,22 @@ echo "info: Provisioning machine"
 #   fi
 # fi
 
-echo "info: Installing zoxide"
-if command -v zoxide &> /dev/null
-then
-  curl -sS https://webinstall.dev/zoxide | bash
-fi
+# echo "info: Installing zoxide"
+# if command -v zoxide &> /dev/null
+# then
+#   curl -sS https://webinstall.dev/zoxide | bash
+# fi
 
-echo "info: Setting up dotfiles via fresh"
-rm -fr ~/.freshrc && rm -fr ~/.fresh && rm -fr  ~/.dotfiles && rm -fr ~/.config/nvim
-FRESH_LOCAL_SOURCE=saimonmoore/fresh_dotfiles bash <(curl -sL get.freshshell.com)
-grep fresh ~/.bashrc || echo "source ~/.fresh/build/shell.sh" >> ~/.bashrc
+# echo "info: Setting up dotfiles via fresh"
+# rm -fr ~/.freshrc && rm -fr ~/.fresh && rm -fr  ~/.dotfiles && rm -fr ~/.config/nvim
+# FRESH_LOCAL_SOURCE=saimonmoore/fresh_dotfiles bash <(curl -sL get.freshshell.com)
+# grep fresh ~/.bashrc || echo "source ~/.fresh/build/shell.sh" >> ~/.bashrc
 
-echo "info: Ensure node + ruby are installed and then install all vim plugins"
-sh -c "curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-nvim +PlugInstall +UpdateRemotePlugins +qall --headless
+# echo "info: Ensure node + ruby are installed and then install all vim plugins"
+# sh -c "curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+# nvim +PlugInstall +UpdateRemotePlugins +qall --headless
 
-echo "info: Installation complete! Now open up a new shell and complete post-installation tasks:"
+# echo "info: Installation complete! Now open up a new shell and complete post-installation tasks:"
 
-echo "info: Install all tmux plugins"
-echo "run: In tmux, hit prefix + I to install plugins"
+# echo "info: Install all tmux plugins"
+# echo "run: In tmux, hit prefix + I to install plugins"
