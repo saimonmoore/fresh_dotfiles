e6516f42c80375f19b5f5a5e760ebea808dc6784
Saimon Moore <saimonmoore@gmail.com>
2021-11-22 16:26:07 +0100

    Fix editor task

--- system/setup.sh
+++ system/setup.sh
@@ -3,10 +3,9 @@
 function install_ansible() {
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt update
-    sudo apt install software-properties-common
+    sudo apt -y install software-properties-common
     sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo add-apt-repository ppa:neovim-ppa/unstable
-    sudo apt install ansible
+    sudo apt -y install ansible
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     which brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew install ansible
