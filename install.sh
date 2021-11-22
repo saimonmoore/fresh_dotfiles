e398cf98e6a0965b225f1e898e1906174c684f85
Saimon Moore <saimonmoore@gmail.com>
2021-11-22 16:02:32 +0100

    Temporary disable nvm/rbenv config

--- install.sh
+++ install.sh
@@ -8,32 +8,31 @@ cd dotfiles/system
 echo "info: Provisioning machine"
 ./setup.sh
 
-echo "info: Installing nvm"
-rm -fr ~/.nvm
-if [ ! -d "$HOME/.nvm" ]
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-  cd ~/.nvm
-  . ./nvm.sh
-  cd -
-  declare -a nodeVersions=("lts/erbium" "lts/ferbium")
-  for version in "${nodeVersions[@]}"
-  do
-     nvm install $version
-     npm install -g yarn
-  done
-fi
+# echo "info: Installing nvm"
+# if [ ! -d "$HOME/.nvm" ]
+#   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+#   cd ~/.nvm
+#   . ./nvm.sh
+#   cd -
+#   declare -a nodeVersions=("lts/erbium" "lts/ferbium")
+#   for version in "${nodeVersions[@]}"
+#   do
+#      nvm install $version
+#      npm install -g yarn
+#   done
+# fi
 
-echo "info: Installing rbenv"
-if [ ! -d "$HOME/.rbenv" ]
-then
-  curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-  if ! grep -q rbenv ~/.bash_profile; then
-    echo 'export PATH=$PATH:~/.rbenv/bin' >> ~/.bash_profile
-    echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-    eval "$(rbenv init -)"
-    rbenv install ruby 2.7.4
-  fi
-fi
+# echo "info: Installing rbenv"
+# if [ ! -d "$HOME/.rbenv" ]
+# then
+#   curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+#   if ! grep -q rbenv ~/.bash_profile; then
+#     echo 'export PATH=$PATH:~/.rbenv/bin' >> ~/.bash_profile
+#     echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+#     eval "$(rbenv init -)"
+#     rbenv install ruby 2.7.4
+#   fi
+# fi
 
 echo "info: Installing zoxide"
 if command -v zoxide &> /dev/null
