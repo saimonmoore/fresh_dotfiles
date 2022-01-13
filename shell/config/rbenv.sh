ceec0007f935ec89d645e2dd67ac89301324dad9
Saimon Moore <saimonmoore@gmail.com>
2022-01-13 16:52:19 +0100

    fix rbenv

--- shell/config/rbenv.sh
+++ shell/config/rbenv.sh
@@ -1,3 +1,2 @@
-command -v ~/.rbenv/bin/rbenv && export PATH=~/.rbenv/bin:$PATH
-command -v ~/.rbenv/bin/rbenv && ~/.rbenv/bin/rbenv init || /usr/local/bin/rbenv init
+/usr/local/bin/rbenv init
 eval "$(rbenv init -)"
